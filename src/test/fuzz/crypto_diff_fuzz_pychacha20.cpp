bbd023a2b379dd97b655ea5b2f7555b039d7ee3b
stratospher <44024636+stratospher@users.noreply.github.com>
2021-12-26 22:21:36 +0530

    fork() too much happens  prolly memory over

--- src/test/fuzz/crypto_diff_fuzz_pychacha20.cpp
+++ src/test/fuzz/crypto_diff_fuzz_pychacha20.cpp
@@ -21,9 +21,11 @@ static void send_msg(int write_fd, std::string msg)
     unsigned iBuf = 0;
     while (iBuf < 4)
     {
+        std::cout<<"::write(write_fd="<<write_fd<<", msgSizeBuf + iBuf="<<iBuf<<"sizeof(msgSizeBuf) - iBuf="<<sizeof(msgSizeBuf) - iBuf<<")\n";
         ssize_t rc = ::write(write_fd, msgSizeBuf + iBuf, sizeof(msgSizeBuf) - iBuf);
         if ( rc < 0 )
         {
+            std::cout << "rc ="<<rc<<std::endl;
             std::cout << "Error writing message size" << std::endl;
             ::exit(1);
         }
@@ -37,57 +39,23 @@ static void send_msg(int write_fd, std::string msg)
             iBuf += rc;
         }
     }
-
-    iBuf = 0;
-    const char *msgBuf = msg.c_str();
-    while (iBuf < msgSize)
-    {
-        ssize_t rc = ::write(write_fd, msgBuf + iBuf, msgSize - iBuf);
-        if ( rc < 0 )
-        {
-            std::cout << "Error writing message" << std::endl;
-            ::exit(1);
-        }
-        else if ( rc == 0 )
-        {
-            std::cout << "rc == 0, what does that mean?" << std::endl;
-            ::exit(1);
-        }
-        else
-        {
-            iBuf += rc;
-        }
-    }
 }
-
-static std::string read_string(int read_fd, uint32_t sz)
-{
-    std::vector<char> msgBuf( sz + 1 );
-    msgBuf[ sz ] = '\0';
-    unsigned iBuf = 0;
-
-    while (iBuf < sz)
-    {
-        ssize_t rc = ::read(read_fd, &(msgBuf[0]) + iBuf, sz - iBuf);
-
-        if ( rc == 0 )
-        {
-            std::cout << __func__ << "@" << __LINE__ << ":::EOF read" << std::endl;
-            exit(1);
-        }
-        else if ( rc < 0 )
-        {
-            std::cout << __func__ << "@" << __LINE__ << ":::Read ERROR during message" << std::endl;
-            exit(1);
-        }
-        else
-        {
-            iBuf += rc;
-        }
-    }
-
-    return std::string( &(msgBuf[0]) );
-}
+//
+//std::vector<unsigned char> intToBytes(int value)
+//{
+//    std::string str="";
+//    std::vector<unsigned char> result;
+//    result.push_back(value >> 24);
+//    result.push_back(value >> 16);
+//    result.push_back(value >>  8);
+//    result.push_back(value      );
+//
+//    for(size_t i=0; i<4; i++){
+//        str+=result[i];
+//    }
+//
+//    return result;
+//}
 
 FUZZ_TARGET(crypto_diff_fuzz_pychacha20)
 {
@@ -107,93 +75,79 @@ FUZZ_TARGET(crypto_diff_fuzz_pychacha20)
 //        ::close(pipe_py_to_cpp[0]);
 //        ::close(pipe_cpp_to_py[1]);
         std::ostringstream oss;
-        oss << "export PY_READ_FD=" << pipe_cpp_to_py[0] << " && "
-            << "export PY_WRITE_FD=" << pipe_py_to_cpp[1] << " && "
+        oss << "export PY_READ_FD=" << pipe_cpp_to_py[1] << " && " //This we need
+            << "export PY_WRITE_FD=" << pipe_py_to_cpp[0] << " && "
             << "export PYTHONUNBUFFERED=true && " // Force stdin, stdout and stderr to be totally unbuffered.
-            << "python3 script.py";
+            << "python3 src/test/fuzz/script.py";
         ::system(oss.str().c_str());
+        std::cout<<"cpp: sent os to python="<<oss.str()<<"*\n";
 //        ::close(pipe_py_to_cpp[1]);
 //        ::close(pipe_cpp_to_py[0]);
+
     }else if (pid < 0){
         std::cout << "Fork failed." << std::endl;
         ::exit(1); //TODO
-    }
+    }else{
 
-    if (fuzzed_data_provider.ConsumeBool()) {
-        const std::vector<unsigned char> key = ConsumeFixedLengthByteVector(fuzzed_data_provider, fuzzed_data_provider.ConsumeIntegralInRange<size_t>(16, 32));
-        chacha20 = ChaCha20{key.data(), key.size()};
-        if(pid == 0){
-            // child: send to our python script [4][init][key.size][key.data] to call ChaCha20PRF(key, 0)
-            std::ostringstream os;
-            std::string key_str="";
-            for(size_t i=0; i<key.size(); i++){
-                key_str+=key[i];
-            }
-            os<<"4init"<<key.size()<<key_str;
-            send_msg(pipe_cpp_to_py[1], os.str()); //TODO write from cpp to python
-        }
+    //Do everything here -- that is everything you'd possibly want to send python
+
+    /* *************************** Fancy separator *************************** */
+
+//        if (fuzzed_data_provider.ConsumeBool()) {
+    const std::vector<unsigned char> key = ConsumeFixedLengthByteVector(fuzzed_data_provider, fuzzed_data_provider.ConsumeIntegralInRange<size_t>(16, 32));
+    chacha20 = ChaCha20{key.data(), key.size()};
+    std::cout<<"cpp: 1. done init chacha20 with key.size()"<<key.size()<<"\n";
+    // child: send to our python script [4][init][key.size][key.data] to call ChaCha20PRF(key, 0)
+    std::ostringstream os;
+    std::string key_str="";
+    for(size_t i=0; i<key.size(); i++){
+        key_str+=key[i];
     }
+    os<<4<<"init"<<key.size()<<key_str;
+    std::cout<<"cpp: sent os to python="<<os.str()<<"*\n";
+    std::cout<<"cpp: fd is"<<pipe_cpp_to_py[1]<<"*\n";
+    send_msg(pipe_cpp_to_py[1], os.str()); //TODO write from cpp to python
+    std::cout<<"cpp: sent over*\n";
+
+//        }
 
     LIMITED_WHILE (fuzzed_data_provider.ConsumeBool(), 3000) {
-    CallOneOf(
-            fuzzed_data_provider,
-//            [&] {
-//                const std::vector<unsigned char> key = ConsumeFixedLengthByteVector(fuzzed_data_provider, fuzzed_data_provider.ConsumeIntegralInRange<size_t>(16, 32));
-//                chacha20.SetKey(key.data(), key.size());
-//                // send to our python script [6][setkey][key.size][key.data] to call ChaCha20PRF(key, 0, 0) # TODO: Maybe make a separate fxn
-//            },
-//            [&] {
-//                uint64_t iv = fuzzed_data_provider.ConsumeIntegral<uint64_t>();
-//                chacha20.SetIV(iv);
-//                // send to our python script [2]["iv"][iv] to call # TODO: Make function
-//            },
-//            [&] {
-//                uint64_t counter = fuzzed_data_provider.ConsumeIntegral<uint64_t>();
-//                chacha20.Seek(counter);
-//                // send to our python script [3]["ctr"][counter] to call # TODO: Make function
-//            },
-            [&] {
-                uint32_t integralInRange = fuzzed_data_provider.ConsumeIntegralInRange<size_t>(0, 4096);
-                std::vector <uint8_t> output(integralInRange);
-                chacha20.Keystream(output.data(), output.size());
-                if (pid == 0) {
-                    // child: send to our python script [6][stream][size] # TODO: Should the Python API return so and so output.data?
+        CallOneOf(
+                fuzzed_data_provider,
+                [&] {
+                    uint32_t integralInRange = fuzzed_data_provider.ConsumeIntegralInRange<size_t>(0, 4096);
+                    std::vector <uint8_t> output(integralInRange);
+                    chacha20.Keystream(output.data(), output.size());
+                    std::cout<<"cpp: 2. done keystream computation chacha20 with output.size()"<<output.size()<<"\n";
+                    std::string output_as_string(output.begin(), output.end());
+                    // child: send to our python script [6][stream][size][string] # TODO: Should the Python API return so and so output.data?
                     std::ostringstream os;
-                    os << "6stream" << integralInRange;
+                    os << 6<<"stream"<<integralInRange<<output_as_string;
                     send_msg(pipe_cpp_to_py[1], os.str()); //TODO write from cpp to python
-                } else {
-                    // parent: receive [size][output] from python pipe
-                    std::string output2 = read_string(pipe_py_to_cpp[0],
-                                                      integralInRange); //TODO read from python to cpp
-                    // compare if they're the same
-                    std::string output_string(output.begin(), output.end());
-                    assert(output_string == output2);
-                }
-            },
-            [&] {
-                uint32_t integralInRange = fuzzed_data_provider.ConsumeIntegralInRange<size_t>(0, 4096);
-                std::vector <uint8_t> output(integralInRange);
-                const std::vector <uint8_t> input = ConsumeFixedLengthByteVector(fuzzed_data_provider, output.size());
-                chacha20.Crypt(input.data(), output.data(), input.size());
-                if (pid == 0) {
+                },
+                [&] {
+                    uint32_t integralInRange = fuzzed_data_provider.ConsumeIntegralInRange<size_t>(0, 4096);
+                    std::vector <uint8_t> output(integralInRange);
+                    const std::vector <uint8_t> input = ConsumeFixedLengthByteVector(fuzzed_data_provider, output.size());
+                    chacha20.Crypt(input.data(), output.data(), input.size());
+                    std::cout<<"cpp: 3. done Crypt computation chacha20 with output.size()"<<input.size()<<"\n";
+                    std::string output_as_string(output.begin(), output.end());
                     // child: send to put python script [5][crypt][size][msg] # TODO: Should the Python API return so and so output.data?
                     std::ostringstream os;
                     std::string msg = "";
                     for (size_t i = 0; i < input.size(); i++) {
                         msg += input[i];
                     }
-                    os << "5crypt" << integralInRange << msg;
+                    os << 5<<"crypt" << integralInRange << msg << output_as_string;
                     send_msg(pipe_cpp_to_py[1], os.str()); //TODO write from cpp to python
-                } else {
-                    // parent: receive [size][output] from python pipe
-                    std::string output2 = read_string(pipe_py_to_cpp[0], integralInRange);//TODO read from python to cpp
-                    // compare if they're the same
-                    std::string output_string(output.begin(), output.end());
-                    assert(output_string == output2);
-                }
-            });
+                });
     }
 
+    /* *************************** End fancy separator *************************** */
+    }
+    //TODO: wait??
+    int returnStatus;
+    waitpid(pid, &returnStatus, 0);
     // child: send to put python script [4][exit] # i think it's needed to open a new pipe.
     ::close(pipe_py_to_cpp[0]);
     ::close(pipe_py_to_cpp[1]);
--- src/test/fuzz/script.py
+++ src/test/fuzz/script.py
@@ -1,21 +1,24 @@
 import os
 import time
 import struct
-import ../../../test/functional/test_framework/chacha20
+import sys
+sys.path.insert(0, '/Users/ruhithomas/Documents/bitcoin/test/functional/test_framework')
+import chacha20
 
 
 _r_fd = int(os.getenv("PY_READ_FD"))
 _w_fd = int(os.getenv("PY_WRITE_FD"))
 
 
-_r_pipe = os.fdopen(_r_fd, 'rb', 0)
-_w_pipe = os.fdopen(_w_fd, 'wb', 0)
+_w_pipe = os.fdopen(_r_fd, 'rb', 0)
+_r_pipe = os.fdopen(_w_fd, 'wb', 0)
 
 
 def _read_n(f, n):
+    print('n is',n)
     buf = ''
     while n > 0:
-        data = f.read(n)
+        data = f.read(n).decode()
         if data == '':
             raise RuntimeError('unexpected EOF')
         buf += data
@@ -28,38 +31,44 @@ def main():
     # TODO: Are these r,w pipes correct?
     while True:
         # get from C++
-        req = _read_n(_r_pipe, 4)
+        # try:
+        time.sleep(2)
+        print("py:_r_fd is**",_r_fd)
+        print("py:_w_fd is",_w_fd)
+        req = _read_n(_w_pipe, 4)
+        # except RuntimeError:
+        #     continue
+        print("py: read req =", req,"*")
         cmd_size = struct.unpack('<I', req)[0]
-        cmd = _read_n(_r_pipe, cmd_size)
+        print("py: cmd_size = ", cmd_size,"*")
+        cmd = _read_n(_w_pipe, cmd_size)
+        print("py: cmd = ", cmd)
         if cmd == "__BAD API__": # TODO: Is this correct. Do we need to do this in the if-else?
             raise Exception(cmd)
 
         # based on request, make response
         if(cmd == "init"):
-            key_details = _read_n(_r_pipe, 4)
+            key_details = _read_n(_w_pipe, 4)
             key_size = struct.unpack('<I', key_details)[0]
-            key = _read_n(_r_pipe, key_size)
+            key = _read_n(_w_pipe, key_size)
             rng = ChaCha20PRF(key, 0)
+            print("py: 1. done with ChaCha20 init", key)
         elif (cmd == "stream"):
-            stream_details = _read_n(_r_pipe, 4)
+            stream_details = _read_n(_w_pipe, 4)
             stream_size = struct.unpack('<I', stream_details)[0]
+            stream_from_cpp = _read_n(_w_pipe, stream_size)
             outres = bytearray(stream_size)
             outres = rng.encrypt(outres)
-
-            # we need to send this to c++
-            msg_size = struct.pack('<I', stream_size)
-            _w_pipe.write(msg_size)
-            _w_pipe.write(outres)
+            assert(outres == stream_from_cpp)
+            print("Yo!!!! stream match!")
         elif (cmd == "crypt"):
-            crypt_details = _read_n(_r_pipe, 4)
+            crypt_details = _read_n(_w_pipe, 4)
             crypt_size = struct.unpack('<I', crypt_details)[0]
-            msg = _read_n(_r_pipe, crypt_size)
+            msg = _read_n(_w_pipe, crypt_size)
+            stream_from_cpp = _read_n(_w_pipe, crypt_size)
             outres = rng.encrypt(msg)
-
-            # we need to send this to c++
-            msg_size = struct.pack('<I', stream_size)
-            _w_pipe.write(msg_size)
-            _w_pipe.write(outres)
+            assert(outres == stream_from_cpp)
+            print("Yo!!!! crypt match!")
         elif (cmd == "exit"):
             _r_pipe.close()
             _w_pipe.close()
