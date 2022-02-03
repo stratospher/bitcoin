6ac289708b082d49ea7cb9ee5257d894ca5825f4
stratospher <44024636+stratospher@users.noreply.github.com>
2022-02-03 19:34:09 +0530

    add outbound tests and clean up tests

--- test/functional/p2p_encrypted.py
+++ test/functional/p2p_encrypted.py
@@ -7,11 +7,8 @@ Test encrypted v2 p2p proposed in BIP 324
 """
 # Imports should be in PEP8 ordering (std library first, then third party
 # libraries then local imports).
-from collections import defaultdict
 
 # Avoid wildcard * imports
-from test_framework.blocktools import (create_block, create_coinbase)
-from test_framework.messages import CInv, MSG_BLOCK
 from test_framework.p2p import (
     P2PInterface,
     msg_block,
@@ -19,37 +16,7 @@ from test_framework.p2p import (
     p2p_lock,
 )
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_equal,
-)
-
-# P2PInterface is a class containing callbacks to be executed when a P2P
-# message is received from the node-under-test. Subclass P2PInterface and
-# override the on_*() methods if you need custom behaviour.
-# class BaseNode(P2PInterface):
-#     def __init__(self):
-#         """Initialize the P2PInterface
-# 
-#         Used to initialize custom properties for the Node that aren't
-#         included by default in the base class. Be aware that the P2PInterface
-#         base class already stores a counter for each P2P message type and the
-#         last received message of each type, which should be sufficient for the
-#         needs of most tests.
-# 
-#         Call super().__init__() first for standard initialization and then
-#         initialize custom properties."""
-#         super().__init__()
-#         # Stores a dictionary of all blocks received
-#         self.block_receive_map = defaultdict(int)
-# 
-# 
-# def custom_function():
-#     """Do some custom behaviour
-# 
-#     If this function is more generally useful for other tests, consider
-#     moving it to a module in test_framework."""
-#     # self.log.info("running custom_function")  # Oops! Can't run self.log outside the BitcoinTestFramework
-#     pass
+from test_framework.util import check_node_connections
 
 class P2PEncrypted(BitcoinTestFramework):
     def set_test_params(self):
@@ -57,45 +24,56 @@ class P2PEncrypted(BitcoinTestFramework):
         self.num_nodes = 1
         self.extra_args = [["-v2transport=1"]]
 
-    # def setup_network(self):
-    #     self.setup_nodes()
-    #     self.connect_nodes(0, 1)
-    #     self.sync_all(self.nodes[0:2])
-
-    # Use setup_nodes() to customize the node start behaviour (for example if
-    # you don't want to start all nodes at the start of the test).
-    # def setup_nodes():
-    #     pass
-
-    # def custom_method(self):
-    #     """Do some custom behaviour for this test
-    # 
-    #     Define it in a method here because you're going to use it repeatedly.
-    #     If you think it's useful in general, consider moving it to the base
-    #     BitcoinTestFramework class so other tests can use it."""
-    # 
-    #     self.log.info("Running custom_method")
-
     def run_test(self):
-        self.encrypted_p2p_v2_test()
+        self.check_inbound_p2p_v2_test()
+        self.check_outbound_p2p_v2_test()
         # self.check_p2p_v2_downgrade_test()
 
     # CASE 1:
     #         Here,
     #             TestNode(RESPONDER) <----------inbound P2PConn---------- INITIATOR
     #
-    def encrypted_p2p_v2_test(self):
+    def check_inbound_p2p_v2_test(self):
         # import pdb; pdb.set_trace()
         node = self.nodes[0]
-        self.log.info("Start a v2 inbound connection")
         peer_id = 0
-        p2p_conn = node.add_p2p_connection(P2PInterface(), wait_for_verack=True, support_v2_p2p=True)
         # assert False
         # p2p_conn.send_and_ping(msg_getdata())
-        # peer = node.add_outbound_p2p_connection(P2PInterface(), p2p_idx=peer_id, support_v2_p2p=True)
+        self.log.info("Inbound connection: TestNode(responder) and P2PInterface(initiator)")
+        self.log.info("TestNode(responder) supports v2 and P2PInterface(initiator) supports v2")
+        p2p_conn = node.add_p2p_connection(P2PInterface(), wait_for_verack=True, support_v2_p2p=True)
+        self.log.info("TestNode(responder) supports v2 and P2PInterface(initiator) supports v1")
+        p2p_conn = node.add_p2p_connection(P2PInterface(), wait_for_verack=True, support_v2_p2p=False)
+        check_node_connections(node=self.nodes[0], num_in=2, num_out=0)
 
-    def check_p2p_v2_downgrade_test(self):
-        pass
+        self.log.info("TestNode(responder) supports v1 and P2PInterface(initiator) supports v2")
+        self.restart_node(0, ["-v2transport=0"])
+        p2p_conn = node.add_p2p_connection(P2PInterface(), wait_for_verack=True, support_v2_p2p=True)
+        self.log.info("TestNode(responder) supports v1 and P2PInterface(initiator) supports v1")
+        p2p_conn = node.add_p2p_connection(P2PInterface(), wait_for_verack=True, support_v2_p2p=False)
+        check_node_connections(node=self.nodes[0], num_in=2, num_out=0)
+        # assert False
+
+    # CASE 2:
+    #         Here,
+    #             TestNode(INITIATOR) ----------outbound P2PConn----------> RESPONDER
+    #
+    def check_outbound_p2p_v2_test(self):
+        node = self.nodes[0]
+        self.restart_node(0, ["-v2transport=1"])
+        self.log.info("Outbound connection: TestNode(initiator) and P2PInterface(responder)")
+        self.log.info("TestNode(Initiator) supports v2 and P2PInterface(responder) supports v2")
+        peer = node.add_outbound_p2p_connection(P2PInterface(), p2p_idx=0, support_v2_p2p=True)
+        self.log.info("TestNode(Initiator) supports v2 and P2PInterface(responder) supports v1")
+        peer = node.add_outbound_p2p_connection(P2PInterface(), p2p_idx=1, support_v2_p2p=False) # not needed
+        check_node_connections(node=self.nodes[0], num_in=0, num_out=2)
+
+        self.restart_node(0, ["-v2transport=0"])
+        self.log.info("TestNode(Initiator) supports v1 and P2PInterface(responder) supports v2")
+        peer = node.add_outbound_p2p_connection(P2PInterface(), p2p_idx=2, support_v2_p2p=True)
+        self.log.info("TestNode(Initiator) supports v1 and P2PInterface(responder) supports v1")
+        peer = node.add_outbound_p2p_connection(P2PInterface(), p2p_idx=3, support_v2_p2p=False)
+        check_node_connections(node=self.nodes[0], num_in=0, num_out=2)
 
 if __name__ == '__main__':
     P2PEncrypted().main()
