7a87e47b10215a725ec2682f8b6fad8ff6b8f8b1
stratospher <44024636+stratospher@users.noreply.github.com>
2022-02-05 14:08:49 +0530

    [test] Construct class to handle v2 P2P protocol functions
    The class `V2P2PEncryption` stores the 4 32-byte keys, session id,
    transport version, whether it's an initiator/responder, whether the
    initial handshake has been completed etc.. It also contains functions
    to perform the initial handshake and to encrypt/decrypt p2p v2 messages.
    
    Using this in `P2PConnection` will help implement v2 P2P protocol.
    See BIP 324 for more details.

--- test/functional/test_framework/v2_p2p.py
+++ test/functional/test_framework/v2_p2p.py
@@ -4,17 +4,179 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Class for v2 P2P protocol (see BIP 324)"""
 
+import os
 import unittest
 
-from .key import SECP256K1, TaggedHash
+from .chacha20_poly1305_ae import ChaCha20Poly1305AE
+from .ellsq import ellsq_decode, ellsq_encode
+from .key import hkdf_expand, hkdf_extract, ECKey, SECP256K1, TaggedHash
+
+MAGIC_BYTES = {
+    "regtest": b"\xfa\xbf\xb5\xda"   # regtest
+}
+
+SHORTID = {
+    13 : b"addr",
+    14 : b"block",
+    15 : b"blocktxn",
+    16 : b"cmpctblock",
+    17 : b"feefilter",
+    18 : b"filteradd",
+    19 : b"filterclear",
+    20 : b"filterload",
+    21 : b"getaddr",
+    22 : b"getblocks",
+    23 : b"getblocktxn",
+    24 : b"getdata",
+    25 : b"getheaders",
+    26 : b"headers",
+    27 : b"inv",
+    28 : b"mempool",
+    29 : b"merkleblock",
+    30 : b"notfound",
+    31 : b"ping",
+    32 : b"pong",
+    33 : b"sendcmpct",
+    34 : b"sendheaders",
+    35 : b"tx",
+    36 : b"verack",
+    37 : b"version",
+    38 : b"getcfilters",
+    39 : b"cfilter",
+    40 : b"getcfheaders",
+    41 : b"cfheaders",
+    42 : b"getcfcheckpt",
+    43 : b"cfcheckpt",
+    44 : b"wtxidrelay",
+    45 : b"addrv2",
+    46 : b"sendaddrv2",
+}
+
+def GetShortIDFromMessageType(msgtype):
+    """ Returns message type ID for the P2P message type as a byte
+    """
+    msgtype_to_shortid=dict(map(reversed, SHORTID.items()))
+    assert (msgtype in msgtype_to_shortid)
+    return msgtype_to_shortid[msgtype].to_bytes(1, 'big')
 
 class V2P2PEncryption:
+    """A class for representing v2 P2P protocol functions.
+
+    This class contains functions to :
+
+    - generate a pair containing private key and elligator-squared encoding of pubkey
+    - perform the initial handshake to instantiate the encrypted transport
+    - encrypt/decrypt v2 P2P messages
+    """
+    def __init__(self, **kwargs):
+        self.initiating = kwargs['initiating'] # True if initiator
+        self.enc_chacha20poly1305ae = None # Chacha20Poly1305 AE instance used for encryption
+        self.dec_chacha20poly1305ae = None # Chacha20Poly1305 AE instance used for decryption
+        self.send_F = None
+        self.send_V = None
+        self.recv_F = None
+        self.recv_V = None
+        self.sid = None
+        self.__privkey = None
+        self.ellsq_pubkey = None
+        self.tried_v2_handshake = False # True when the initial handshake is over
+
     @staticmethod
     def ellsq_ecdh_secret(priv, ellsq_X, ellsq_Y, pub_theirs):
         pub_theirs = pub_theirs.get_group_element()
         pub_theirs = pub_theirs[0].val, pub_theirs[1].val, 1
         xonly_ecdh, _, _ = SECP256K1.affine(SECP256K1.mul([(pub_theirs, priv.secret)]))
         return TaggedHash("secp256k1_ellsq_xonly_ecdh", ellsq_X + ellsq_Y + xonly_ecdh.to_bytes(32, 'big')).hex()
+
+    def v2_keygen(self):
+        """ Generates private key and elligator-squared encoding of pubkey
+        Returns: ECKey, 64 bytes encoding
+        """
+        while True:
+            priv = ECKey()
+            priv.generate()
+            pub = priv.get_pubkey()
+            encoded_pubkey = ellsq_encode(pub, os.urandom(32))
+            if not (self.initiating and encoded_pubkey[:12] == MAGIC_BYTES["regtest"] + b"version\x00"):
+                return priv, encoded_pubkey
+
+    def initiate_v2_handshake(self):
+        """ Initiator initiates the handshake by sending an unencrypted ellsq encoding of public key to responder
+        """
+        assert self.initiating
+        self.__privkey, self.ellsq_pubkey = self.v2_keygen()
+        return self.ellsq_pubkey
+
+    def respond_v2_handshake(self, ellsq_X):
+        """ Responder on receiving the unencrypted ellsq encoding of public key - decodes it, computes ECDH secret and
+        instantiates the encrypted transport. It sends back to the initiator an unencrypted ellsq encoding of public key
+        """
+        assert not self.initiating
+        initiator_pubkey = ellsq_decode(ellsq_X)
+        self.__privkey, self.ellsq_pubkey = self.v2_keygen()
+        ecdh_secret = V2P2PEncryption.ellsq_ecdh_secret(self.__privkey, initiator_pubkey, self.ellsq_pubkey, initiator_pubkey)
+        self.initialize_v2_transport(ecdh_secret, ellsq_X, self.ellsq_pubkey)
+        return self.ellsq_pubkey
+
+    def initiator_complete_handshake(self, response):
+        """ Initiator on receiving the unencrypted ellsq encoding of public key - decodes it, computes ECDH secret and
+        instantiates the encrypted transport.
+        """
+        assert self.initiating
+        ellsq_Y = response[:64]
+        Y = ellsq_decode(ellsq_Y)
+        ecdh_secret = V2P2PEncryption.ellsq_ecdh_secret(self.__privkey, self.ellsq_pubkey, ellsq_Y, Y)
+        self.initialize_v2_transport(ecdh_secret, self.ellsq_pubkey, ellsq_Y)
+
+    def initialize_v2_transport(self, ecdh_secret, ellsq_X, ellsq_Y):
+        salt = b"bitcoin_v2_shared_secret" + ellsq_X + ellsq_Y + MAGIC_BYTES["regtest"]
+        prk = hkdf_extract(salt, bytes.fromhex(ecdh_secret))
+        del ecdh_secret # We no longer need the ECDH secret
+
+        initiator_F = hkdf_expand(prk, b"initiator_F")
+        initiator_V = hkdf_expand(prk, b"initiator_V")
+        responder_F = hkdf_expand(prk, b"responder_F")
+        responder_V = hkdf_expand(prk, b"responder_V")
+        self.sid    = hkdf_expand(prk, b"session_id")
+
+        if self.initiating:
+            self.send_F = initiator_F
+            self.send_V = initiator_V
+            self.recv_F = responder_F
+            self.recv_V = responder_V
+        else:
+            self.recv_F = initiator_F
+            self.recv_V = initiator_V
+            self.send_F = responder_F
+            self.send_V = responder_V
+
+        self.enc_chacha20poly1305ae = ChaCha20Poly1305AE(self.send_F, self.send_V)
+        self.dec_chacha20poly1305ae = ChaCha20Poly1305AE(self.recv_F, self.recv_V)
+
+    def v2_enc_msg(self, msg_bytes, ignore=False):
+        """ Encrypts msg_bytes(which is the payload).
+        Returns:
+            Encrypted message consisting of 3 bytes length + payload + 16 bytes MAC
+        """
+        header = payload_len = len(msg_bytes)
+        if ignore:
+            header = header | (1 << 23)
+        msg_bytes = header.to_bytes(3, byteorder='little') + msg_bytes
+        _, ret = self.enc_chacha20poly1305ae.crypt(True, msg_bytes, payload_len)
+        return ret
+
+    def v2_dec_msg(self, encrypted_bytes):
+        """ Decrypts encrypted_bytes which consists of 3 bytes length + payload + 16 bytes MAC
+        Returns:
+            Plaintext message which contains the payload
+        """
+        ignore, length = self.dec_chacha20poly1305ae.decrypt_length(encrypted_bytes)
+        disconnect, ret = self.dec_chacha20poly1305ae.crypt(False, encrypted_bytes, length)
+        if disconnect:
+            return len(ret), None
+        if ignore or len(ret) >= 2**23: # messages need to be rejected - return length to clear recvbuf
+            return len(ret), b""
+        return len(ret), ret
 
 class TestFrameworkP2PEncryption(unittest.TestCase):
     def test_bip324_keys_derivation_test(self):
