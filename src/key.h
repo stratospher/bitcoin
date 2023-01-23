dhruv/bitcoin-core-ci

Enable ECDH computation on secp256k1 keys

src/key.h
@@ -7,11 +7,17 @@
 #ifndef BITCOIN_KEY_H
 #define BITCOIN_KEY_H
 
+#include <crypto/sha256.h>
+#include <hash.h>
 #include <pubkey.h>
 #include <serialize.h>
+#include <span.h>
 #include <support/allocators/secure.h>
 #include <uint256.h>
 
+#include <array>
+#include <cstddef>
+#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -21,6 +27,12 @@
  * (SIZE bytes)
  */
 typedef std::vector<unsigned char, secure_allocator<unsigned char> > CPrivKey;
+
+constexpr static size_t ECDH_SECRET_SIZE = CSHA256::OUTPUT_SIZE;
+const auto HASHER_BIP324_ECDH = TaggedHash("secp256k1_ellsq_xonly_ecdh");
+
+// Used to represent a ECDH secret (ECDH_SECRET_SIZE bytes)
+using ECDHSecret = std::array<std::byte, ECDH_SECRET_SIZE>;
 
 /** An encapsulated private key. */
 class CKey
@@ -156,6 +168,11 @@ public:
 
     //! Load private key and check that public key matches.
     bool Load(const CPrivKey& privkey, const CPubKey& vchPubKey, bool fSkipCheck);
+
+    // Returns false if an invalid public key is provided
+    std::optional<ECDHSecret> ComputeBIP324ECDHSecret(const Span<const std::byte> their_ellswift,
+                                                      const Span<const std::byte> our_ellswift,
+                                                      bool initiating) const;
 };
 
 struct CExtKey {

stratospher/bitcoin

src/key.h
@@ -7,11 +7,17 @@
 #ifndef BITCOIN_KEY_H
 #define BITCOIN_KEY_H
 
+#include <crypto/sha256.h>
+#include <hash.h>
 #include <pubkey.h>
 #include <serialize.h>
+#include <span.h>
 #include <support/allocators/secure.h>
 #include <uint256.h>
 
+#include <array>
+#include <cstddef>
+#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -21,6 +27,12 @@
  * (SIZE bytes)
  */
 typedef std::vector<unsigned char, secure_allocator<unsigned char> > CPrivKey;
+
+constexpr static size_t ECDH_SECRET_SIZE = CSHA256::OUTPUT_SIZE;
+const auto HASHER_BIP324_ECDH = TaggedHash("bip324_ellswift_xonly_ecdh");
+
+// Used to represent a ECDH secret (ECDH_SECRET_SIZE bytes)
+using ECDHSecret = std::array<std::byte, ECDH_SECRET_SIZE>;
 
 /** An encapsulated private key. */
 class CKey
@@ -156,6 +168,11 @@ public:
 
     //! Load private key and check that public key matches.
     bool Load(const CPrivKey& privkey, const CPubKey& vchPubKey, bool fSkipCheck);
+
+    // Returns false if an invalid public key is provided
+    std::optional<ECDHSecret> ComputeBIP324ECDHSecret(const Span<const std::byte> their_ellswift,
+                                                      const Span<const std::byte> our_ellswift,
+                                                      bool initiating) const;
 };
 
 struct CExtKey {