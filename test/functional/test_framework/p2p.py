stratospher/bitcoin-core-ci

[test] Ignore BIP324 decoy messages
Also allow P2PConnection::send_message() to send decoy messages for
writing tests.

test/functional/test_framework/p2p.py
@@ -302,6 +302,8 @@ class P2PConnection(asyncio.Protocol):
                     if msg is None:
                         raise ValueError("invalid v2 mac tag " + repr(self.recvbuf))
                     self.recvbuf = self.recvbuf[msglen:]
+                    if msg == b'': # reject decoy messages and messages longer than permitted payload length
+                        return
                     size_or_shortid = msg[0]
                     if 0 < size_or_shortid <= 12:
                         # a string command
@@ -350,12 +352,12 @@ class P2PConnection(asyncio.Protocol):
 
     # Socket write methods
 
-    def send_message(self, message):
+    def send_message(self, message, is_decoy=False):
         """Send a P2P message over the socket.
 
         This method takes a P2P payload, builds the P2P header and adds
         the message to the send buffer to be sent over the socket."""
-        tmsg = self.build_message(message)
+        tmsg = self.build_message(message, is_decoy)
         self._log_message("send", message)
         return self.send_raw_message(tmsg)
 
@@ -373,7 +375,7 @@ class P2PConnection(asyncio.Protocol):
 
     # Class utility methods
 
-    def build_message(self, message):
+    def build_message(self, message, is_decoy=False):
         """Build a serialized P2P message"""
         if self.support_v2_p2p:
             msgtype = message.msgtype
@@ -384,7 +386,7 @@ class P2PConnection(asyncio.Protocol):
                 tmsg = len(msgtype).to_bytes(1, 'big')
                 tmsg += msgtype
             tmsg += data
-            return self.v2_connection.v2_enc_packet(tmsg)
+            return self.v2_connection.v2_enc_packet(tmsg, ignore=is_decoy)
         else:
             msgtype = message.msgtype
             data = message.serialize()
@@ -800,7 +802,7 @@ class P2PDataStore(P2PInterface):
         if response is not None:
             self.send_message(response)
 
-    def send_blocks_and_test(self, blocks, node, *, success=True, force_send=False, reject_reason=None, expect_disconnect=False, timeout=60):
+    def send_blocks_and_test(self, blocks, node, *, success=True, force_send=False, reject_reason=None, expect_disconnect=False, timeout=60, is_decoy=False):
         """Send blocks to test node and test whether the tip advances.
 
          - add all blocks to our block_store
@@ -819,9 +821,11 @@ class P2PDataStore(P2PInterface):
 
         reject_reason = [reject_reason] if reject_reason else []
         with node.assert_debug_log(expected_msgs=reject_reason):
+            if is_decoy:  # since decoy messages don't get sent - no need to wait for response
+                force_send = True
             if force_send:
                 for b in blocks:
-                    self.send_message(msg_block(block=b))
+                    self.send_message(msg_block(block=b), is_decoy)
             else:
                 self.send_message(msg_headers([CBlockHeader(block) for block in blocks]))
                 self.wait_until(

stratospher/bitcoin

test/functional/test_framework/p2p.py
@@ -318,6 +318,8 @@ class P2PConnection(asyncio.Protocol):
                     if msglen == -1:
                         raise ValueError("invalid v2 mac tag " + repr(self.recvbuf))
                     self.recvbuf = self.recvbuf[msglen:]
+                    if msg == b'': # reject decoy messages and messages longer than permitted payload length
+                        return
                     shortid = msg[0]
                     if shortid == 0:
                         # a string command
@@ -366,12 +368,12 @@ class P2PConnection(asyncio.Protocol):
 
     # Socket write methods
 
-    def send_message(self, message):
+    def send_message(self, message, is_decoy=False):
         """Send a P2P message over the socket.
 
         This method takes a P2P payload, builds the P2P header and adds
         the message to the send buffer to be sent over the socket."""
-        tmsg = self.build_message(message)
+        tmsg = self.build_message(message, is_decoy)
         self._log_message("send", message)
         return self.send_raw_message(tmsg)
 
@@ -389,7 +391,7 @@ class P2PConnection(asyncio.Protocol):
 
     # Class utility methods
 
-    def build_message(self, message):
+    def build_message(self, message, is_decoy=False):
         """Build a serialized P2P message"""
         if self.supports_v2_p2p:
             msgtype = message.msgtype
@@ -401,7 +403,7 @@ class P2PConnection(asyncio.Protocol):
                 tmsg += msgtype
                 tmsg += b"\x00" * (12 - len(msgtype))
             tmsg += data
-            return self.v2_state.v2_enc_packet(tmsg)
+            return self.v2_state.v2_enc_packet(tmsg, ignore=is_decoy)
         else:
             msgtype = message.msgtype
             data = message.serialize()
@@ -814,7 +816,7 @@ class P2PDataStore(P2PInterface):
         if response is not None:
             self.send_message(response)
 
-    def send_blocks_and_test(self, blocks, node, *, success=True, force_send=False, reject_reason=None, expect_disconnect=False, timeout=60):
+    def send_blocks_and_test(self, blocks, node, *, success=True, force_send=False, reject_reason=None, expect_disconnect=False, timeout=60, is_decoy=False):
         """Send blocks to test node and test whether the tip advances.
 
          - add all blocks to our block_store
@@ -833,9 +835,11 @@ class P2PDataStore(P2PInterface):
 
         reject_reason = [reject_reason] if reject_reason else []
         with node.assert_debug_log(expected_msgs=reject_reason):
+            if is_decoy:  # since decoy messages don't get sent - no need to wait for response
+                force_send = True
             if force_send:
                 for b in blocks:
-                    self.send_message(msg_block(block=b))
+                    self.send_message(msg_block(block=b), is_decoy)
             else:
                 self.send_message(msg_headers([CBlockHeader(block) for block in blocks]))
                 self.wait_until(